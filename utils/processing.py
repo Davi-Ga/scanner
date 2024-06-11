5fca97dc137cd39ed7fd7d28aba3b8434a6b45f2
Davi-Ga <contato@daviga.dev.br>
2024-06-11 12:52:35 -0300

    refactor: Refactor ImageProcessing class to improve code organization and readability

--- utils/processing.py
+++ utils/processing.py
@@ -4,6 +4,15 @@ import numpy as np
 
 
 class ImageProcessing:
+    
+    def resize_image(self, img, dim_limit):
+        """Resizes the image to the specified dimension limit."""
+        max_dim = max(img.shape)
+        if max_dim > dim_limit:
+            resize_scale = dim_limit / max_dim
+            img = cv2.resize(img, None, fx=resize_scale, fy=resize_scale)
+        return img
+    
     def get_blank_page(self, img):
         """Performs morphological operations on the image to remove noise."""
         print('Getting blank page')
@@ -32,18 +41,32 @@ class ImageProcessing:
         return img_output
 
     def edge_detection(self, img):
-        """Converts the image to grayscale, applies Gaussian blur, and then performs edge detection using the Canny algorithm."""
-        print('Detecting edges')
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (11, 11), 0)
-        canny = cv2.Canny(gray, 0, 200)
-        canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
-        return canny
+            """Converts the image to grayscale, applies Gaussian blur, and then performs edge detection using the Canny algorithm."""
+            print('Detecting edges')
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (11, 11), 0)
+            canny = cv2.Canny(gray, 0, 200)
+            canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+            return canny
 
     def get_contours(self, img, canny):
         """Finds and draws contours based on the edges detected by the Canny algorithm."""
-        print('Getting contours')
+        con = np.zeros_like(img)
         contours, hierarchy = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
         page = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
-        img = cv2.drawContours(img, page, -1, (0, 255, 255), 3)
-        return img
+        con = cv2.drawContours(con, page, -1, (0, 255, 255), 3)
+        return con,page
+
+    def get_corners(self, img, page):
+        """Identifies the corners of the largest contour."""
+        print('Getting corners')
+        corners = []
+        for c in page:
+            # Approximate the contour.
+            epsilon = 0.02 * cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, epsilon, True)
+            # If our approximated contour has four points, we have found our corners.
+            if len(approx) == 4:
+                corners = approx
+                break
+        return corners
